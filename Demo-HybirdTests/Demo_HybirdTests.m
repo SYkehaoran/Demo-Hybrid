e5d81c7b259f51b14b1c5e687b08b5213814ffd2
SYkehaoran <18510666543@163.com>
2018-08-29 15:07:51 +0800

    Initial Commit

---
+++ Demo-HybirdTests/Demo_HybirdTests.m
@@ -0,0 +1,39 @@
+//
+//  Demo_HybirdTests.m
+//  Demo-HybirdTests
+//
+//  Created by 柯浩然 on 8/29/18.
+//  Copyright © 2018 柯浩然. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Demo_HybirdTests : XCTestCase
+
+@end
+
+@implementation Demo_HybirdTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
